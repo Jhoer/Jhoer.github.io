9952823eab83434c0e6d6aa4d52994c436c05ef9
Jhoer <Jhoer@users.noreply.github.com>
2025-11-14 20:43:36 -0500

    Delete js/scripts.js

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("contactForm").addEventListener("submit", function(e){
- e.preventDefault();
- const form = e.target;
- const data = new FormData(form);
- fetch("https://docs.google.com/forms/d/e/1FAIpQLSc7l_formResponse", { method:"POST", body:data })
- document.getElementById("msg").innerText="¡Gracias! Tus datos fueron enviados.";
- form.reset();
-});
